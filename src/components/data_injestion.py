4791d033f89a50cdced25e19d1bbea6d371569d9
siddarthpatil <sidwid365@gmail.com>
2023-09-14 10:42:36 +0530

    Exception, logger tested

---
+++ src/components/data_injestion.py
@@ -0,0 +1,10 @@
+# data injestion does the following:
+# 1. read the data from the source
+# 2. save the data in the data/raw for further processing
+# 3. log the data source, size of the data, time of download
+# 4. save the config in the config folder
+# 5. save the data in the database
+# 6. log the database
+# 7. save the database in the database folder
+# 8. save the database in the data/processed folder
+# 9. log the end of the data injestion
